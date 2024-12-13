14c6353401931219d46a19d32385228c77f75f67
HP <madhankumar.m@verandarace.com>
2024-12-13 13:39:39 +0530

    change

--- src/app/pages/contact-us/contact-us.component.ts
+++ src/app/pages/contact-us/contact-us.component.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { CrmApiService } from '../../services/zohoCrm/crm-api.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-contact-us',
@@ -45,5 +46,55 @@ export class ContactUsComponent implements OnInit {
     this.stateLanguage = this.currentState[0].language
     this.districtList = this.currentState[0].districts
   }
+
+  onContactSubmit(data: any) {
+    this.submitClicked = true;
+    console.log(data.value)
+    const obj = {
+      name: data.value.studentName,
+      phonenumber: '+91' + data.value.phoneNumber,
+      email: data.value.emailID,
+      state: data.value.state,
+      district: data.value.district,
+      message: data.value.message,
+    };
+  
+    // Disable the submit button during the API call
+    this.http.post('https://backend.verandarace.com/veranda/iascontactus', obj).subscribe({
+      next: (response: any) => {
+        if (response.success === true) {
+          Swal.fire({
+            icon: "success",
+            title: "Success",
+            text: "We will reach out to you shortly",
+          }).then(() => {
+            this.resetForm(data); // Reset the form instead of reloading
+          });
+        } else {
+          Swal.fire({
+            icon: "error",
+            title: "Error",
+            text: "Please try again",
+          });
+        }
+      },
+      error: () => {
+        Swal.fire({
+          icon: "error",
+          title: "Network Error",
+          text: "Something went wrong. Please try again later.",
+        });
+      },
+      complete: () => {
+        this.submitClicked = false; // Re-enable the submit button
+      },
+    });
+  }
+  
+  resetForm(data: any) {
+    data.reset(); // Reset the form controls
+    this.submitClicked = false;
+  }
+  
 
 }
