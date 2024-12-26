dfdba249f4e541ba2983d1a7cf9318709fe9cca6
Adharsh <adharsh.s@verandarace.com>
2024-12-26 14:17:24 +0530

    new changes

--- src/app/pages/product/payment/orderstatus/orderstatus.component.ts
+++ src/app/pages/product/payment/orderstatus/orderstatus.component.ts
@@ -15,36 +15,36 @@ export class OrderstatusComponent {
     amount: '',
   };
 
-  paymentStatus = 'success';
+  paymentStatus = '';
 
   constructor(
     private http: HttpClient,
     private route: ActivatedRoute,
     private router: Router
   ) {
-    // const orderId = this.route.snapshot.paramMap.get('id');
+    const orderId = this.route.snapshot.paramMap.get('id');
 
-    // if (orderId) {
-    //   this.http
-    //     .post('https://backend.verandaias.com/verandaias/orders/getPayOrder', {
-    //       orderId,
-    //     })
-    //     .subscribe((response: any) => {
-    //       if (response.message.status == 'success') {
-    //         this.http
-    //           .post(
-    //             'https://backend.verandaias.com/verandaias/orders/doAllOrderOperations',
-    //             { orderId }
-    //           )
-    //           .subscribe((data) => {});
-    //         this.paymentDetails = response.message;
-    //         this.paymentStatus = response.message.status;
-    //       } else {
-    //         this.paymentStatus = response.message.status;
-    //       }
-    //     });
-    // } else {
-    //   this.router.navigate(['/']);
-    // }
+    if (orderId) {
+      this.http
+        .post('https://backend.verandaias.com/verandaias/orders/getPayOrder', {
+          orderId,
+        })
+        .subscribe((response: any) => {
+          if (response.message.status == 'success') {
+            this.http
+              .post(
+                'https://backend.verandaias.com/verandaias/orders/doAllOrderOperations',
+                { orderId }
+              )
+              .subscribe((data) => {});
+            this.paymentDetails = response.message;
+            this.paymentStatus = response.message.status;
+          } else {
+            this.paymentStatus = response.message.status;
+          }
+        });
+    } else {
+      this.router.navigate(['/']);
+    }
   }
 }
